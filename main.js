08390b8e61a4a7ce35cff7cbc5e7608285d4ea53
Toni Williams <100317017+toniwilliams1@users.noreply.github.com>
2022-06-04 00:10:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-document.querySelector("button").addEventListener("click", getPlayer);
+ document.querySelector("button").addEventListener("click", getPlayer);
 
 function getPlayer() {
   let name = document.querySelector("input").value;
 
-      fetch(`https://www.balldontlie.io/api/v1/players?search=${name}`)
-
-   .then((res) => res.json()) 
+  fetch(`https://www.balldontlie.io/api/v1/players?search=${name}`)
+    .then((res) => res.json())
     .then((data) => {
-   /*   console.log(data)
-      console.log(data.data[0])
-}) */
-const firstPlayer = data.data[0]
-  document.querySelector(".first-name").innerText = firstPlayer.first_name 
-    document.querySelector(".last-name").innerText = firstPlayer.last_name 
-     document.querySelector(".height-feet").innerText =  firstPlayer.height_feet 
-      document.querySelector(".height-inches").innerText = firstPlayer.height_inches 
-     document.querySelector(".position").innerText = firstPlayer.position
-             document.querySelector(".weight-pounds").innerText =firstPlayer.weight_pounds
+      const firstPlayer = data.data[0];
+      document.querySelector(".first-name").innerText = firstPlayer.first_name;
+      document.querySelector(".last-name").innerText = firstPlayer.last_name;
+      document.querySelector(".height-feet").innerText =
+        firstPlayer.height_feet;
+      document.querySelector(".height-inches").innerText =
+        firstPlayer.height_inches;
+      document.querySelector(".position").innerText = firstPlayer.position;
+      document.querySelector(".weight-pounds").innerText =
+        firstPlayer.weight_pounds;
+      document.querySelector(".name").innerText = firstPlayer.team.name;
+      document.querySelector(".abbreviation").innerText =
+        firstPlayer.team.abbreviation;
+      document.querySelector(".division").innerText = firstPlayer.team.division;
+      document.querySelector(".city").innerText = firstPlayer.team.city;
+
+      document.querySelector(".conference").innerText =
+        firstPlayer.team.conference;
+    });
+}

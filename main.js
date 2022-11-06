6c86230fabc3ca9f456c79be484b5bd7cd9c0baa
Toni Williams <williata04@mail.buffalostate.edu>
2022-11-06 00:30:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function getPlayer() {
     .then((res) => res.json())
     .then((data) => {
       const firstPlayer = data.data[0];
-      document.querySelector(".first-name").innerText = firstPlayer.first_name;
+    document
+    document.querySelector("h3").innerText = `${firstPlayer.team.full_name} player ${firstPlayer.first_name} ${firstPlayer.last_name} is ${firstPlayer.height_feet} feet and ${firstPlayer.height_inches} inches tall; and weighs ${ firstPlayer.weight_pounds} pounds.  He plays for the ${firstPlayer.team.full_name} which is part of the ${firstPlayer.team.division} division and the  ${firstPlayer.team.conference} conference.` ;
       document.querySelector(".last-name").innerText = firstPlayer.last_name;
       document.querySelector(".height-feet").innerText =
         firstPlayer.height_feet;
@@ -16,7 +17,7 @@ function getPlayer() {
       document.querySelector(".position").innerText = firstPlayer.position;
       document.querySelector(".weight-pounds").innerText =
         firstPlayer.weight_pounds;
-      document.querySelector(".name").innerText = firstPlayer.team.name;
+      document.querySelector(".name").innerText = firstPlayer.team.full_name;
       document.querySelector(".abbreviation").innerText =
         firstPlayer.team.abbreviation;
       document.querySelector(".division").innerText = firstPlayer.team.division;
@@ -26,3 +27,4 @@ function getPlayer() {
         firstPlayer.team.conference;
     });
 }
+
